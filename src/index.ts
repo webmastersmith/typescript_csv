9c2a58c102bc2d4d8c9aa18b11e307018bde9e90
Bryon Smith <bryon@smithauto.us>
2021-12-31 20:26:30 -0600

    parse csv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello bryon')
+import * as fs from 'fs'
+
+const data: string[][] = fs
+  .readFileSync('./football.csv', 'utf-8')
+  .split(/\r?\n/)
+  .map((line: string): string[] => line.split(','))
+
+const wins: number = data.filter(
+  (a: string[]): boolean =>
+    (a[1] === 'Man United' && a[5] === 'H') ||
+    (a[2] === 'Man United' && a[5] === 'A')
+).length
+
+console.log(`Man United won ${wins} games.`)
